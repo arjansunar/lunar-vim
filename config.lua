274e10a99ad06998db48ca8f9119e7a029ed1b8c
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-09-29 07:14:33 +0545

    chore: format

--- config.lua
+++ config.lua
@@ -6,28 +6,28 @@
 
 -- PLUGINS
 lvim.plugins = {
-	{
-		"Exafunction/codeium.vim",
-		event = "BufEnter",
-		config = function()
-			vim.keymap.set("i", "<C-g>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true })
-			vim.keymap.set("i", "<M-]>", function()
-				return vim.fn["codeium#CycleCompletions"](1)
-			end, { expr = true })
-			vim.keymap.set("i", "<M-[>", function()
-				return vim.fn["codeium#CycleCompletions"](-1)
-			end, { expr = true })
-			vim.keymap.set("i", "<c-x>", function()
-				return vim.fn["codeium#Clear"]()
-			end, { expr = true })
-		end,
-	},
-	{
-		"mrjones2014/nvim-ts-rainbow",
-	},
+  {
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
+    config = function()
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+      vim.keymap.set("i", "<M-]>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true })
+      vim.keymap.set("i", "<M-[>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true })
+      vim.keymap.set("i", "<c-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true })
+    end,
+  },
+  {
+    "mrjones2014/nvim-ts-rainbow",
+  },
 }
 lvim.builtin.treesitter.rainbow.enable = true
 
-lvim.builtin.nvimtree.setup.view.side ="right"
+lvim.builtin.nvimtree.setup.view.side = "right"
